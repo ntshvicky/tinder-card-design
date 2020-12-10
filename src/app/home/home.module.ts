2faeb9e21d5173443245cbecd3bc859d1b7eba35
Nitish Srivastava <ntsh.vicky@gmail.com>
2020-12-10 23:34:55 +0530

    first one

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { TinderUiComponent } from '../tinder-ui/tinder-ui.component';
 
 @NgModule({
   imports: [
@@ -14,6 +14,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, TinderUiComponent]
 })
 export class HomePageModule {}
